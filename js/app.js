4bbfa7d9eb4f2b12be2bda9ac80f60ea915f1e34
jamestaylr <jamestaylr@gmail.com>
2015-02-07 12:28:09 -0500

    Added Geolocation support
    `getLocation()` now gets the
    `navigator.geolocation.getCurrentPosition()` and calls a callback with
    creates the HTTP with a parametrized JSON request.

--- js/app.js
+++ js/app.js
@@ -3,7 +3,6 @@ var StrapKit = require('strapkit');
 var app_id = "";
 var key = "";
 var radius = 1000;
-var location = "37.226789,-80.422676";
 
 var parseFeed = function(data, quantity) {
     var items = [];
@@ -26,58 +25,89 @@ var parseFeed = function(data, quantity) {
             subtitle: type,
             data: response
         });
+
     }
 
     // Finally return whole array
     return items;
 };
 
+
+function getLocation(cb) {
+
+    if (navigator && navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(position) {
+            console.log("Got Geolocation!");
+
+            var latitude = position.coords.latitude;
+            var longitude = position.coords.longitude;
+
+            cb([latitude, longitude]);
+
+        }, function() {
+            console.log("Failed to get Geolocation!");
+        }, {
+            maximumAge: 60000,
+            timeout: 5000,
+            enableHighAccuracy: true
+        });
+    }
+
+}
+
 StrapKit.Metrics.Init(app_id);
 
-// Make request using Google Places API
-StrapKit.HttpClient({
-        url: 'https://maps.googleapis.com/maps/api/place/search/json?location=' + location + '&radius=' + radius + '&sensor=true&key=' + key,
-        type: 'json'
-    },
-    function(data) {
+getLocation(function(position) {
 
-        var menuItems = parseFeed(data, 4);
+    var location = position[0] + ',' + position[1];
 
-        StrapKit.Metrics.logEvent("/httpClient/success", menuItems);
+    // Make request using Google Places API
+    StrapKit.HttpClient({
+            url: 'https://maps.googleapis.com/maps/api/place/search/json?location=' + location + '&radius=' + radius + '&sensor=true&key=' + key,
+            type: 'json'
+        },
+        function(data) {
 
-        var resultsPage = StrapKit.UI.Page();
-        // Construct Menu to show to user
-        var resultsMenu = StrapKit.UI.ListView({
-            items: menuItems
-        });
+            console.log("Data response was returned from Places API request!");
 
-        // Add an action for SELECT
-        resultsMenu.setOnItemClick(function(e) {
-            var detailPage = StrapKit.UI.Page();
+            var menuItems = parseFeed(data, 6);
 
-            var location = e.item.data;
+            StrapKit.Metrics.logEvent("/httpClient/success", menuItems);
 
-            var detailPage = StrapKit.UI.Page();
-            // Create the Card for detailed view
-            var detailCard = StrapKit.UI.Card({
-                subtitle: e.item.title,
-                body: ""
+            var resultsPage = StrapKit.UI.Page();
+
+            // Construct Menu to show to user
+            var resultsMenu = StrapKit.UI.ListView({
+                items: menuItems
             });
 
-            detailPage.addView(detailCard);
-            detailPage.show();
+            // Add an action for SELECT
+            resultsMenu.setOnItemClick(function(e) {
+                var detailPage = StrapKit.UI.Page();
 
-            StrapKit.Metrics.logEvent("show/detailPage", e.item.data);
-        });
+                var location = e.item.data;
 
-        // Show the Menu, hide the splash
-        resultsPage.addView(resultsMenu);
-        resultsPage.show();
+                var detailPage = StrapKit.UI.Page();
+                // Create the Card for detailed view
+                var detailCard = StrapKit.UI.Card({
+                    subtitle: e.item.title,
+                    body: ""
+                });
 
-        StrapKit.Metrics.logEvent("show/resultsPage");
+                detailPage.addView(detailCard);
+                detailPage.show();
 
-    },
-    function(error) {
-        console.log(error);
-    }
-);
+                StrapKit.Metrics.logEvent("show/detailPage", e.item.data);
+            });
+
+            // Show the Menu, hide the splash
+            resultsPage.addView(resultsMenu);
+            resultsPage.show();
+
+            StrapKit.Metrics.logEvent("show/resultsPage");
+        },
+        function(error) {
+            console.log(error);
+        }
+    );
+});
